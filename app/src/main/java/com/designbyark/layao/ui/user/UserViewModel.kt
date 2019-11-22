5b8cc5981dab3288d1ba6be330ecaefd4f19bf11
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-11-22 20:54:39 +0500

    adding user fragment
    additional actions:
    - refactoring label for bottom bar menu items

---
+++ app/src/main/java/com/designbyark/layao/ui/user/UserViewModel.kt
@@ -0,0 +1,15 @@
+package com.designbyark.layao.ui.user
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is user Fragment"
+    }
+
+    val text: LiveData<String> = _text
+
+}
