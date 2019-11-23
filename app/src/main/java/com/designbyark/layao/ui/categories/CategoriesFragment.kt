68715ffa3c63261a7bb60fb9000078b01004c468
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-11-23 19:53:03 +0500

    adding recycler-view to categories fragment

--- app/src/main/java/com/designbyark/layao/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/categories/CategoriesFragment.kt
@@ -12,20 +12,14 @@ import com.designbyark.layao.R
 
 class CategoriesFragment : Fragment() {
 
-    private lateinit var categoriesViewModel: CategoriesViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        categoriesViewModel =
-            ViewModelProviders.of(this).get(CategoriesViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_categories, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        categoriesViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
     }
 }
