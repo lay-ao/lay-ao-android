c9e0957e94e740441c17bb138d4842a3d0ac438a
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-26 20:13:27 +0500

    adding favourites model class and view holder

--- app/src/main/java/com/designbyark/layao/ui/favorites/FavoritesViewHolder.kt
+++ app/src/main/java/com/designbyark/layao/ui/favorites/FavoritesViewHolder.kt
@@ -1,4 +1,50 @@
 package com.designbyark.layao.ui.favorites
 
-class FavoritesViewHolder {
+import android.content.Context
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.designbyark.layao.R
+import com.designbyark.layao.common.circularProgressBar
+import com.designbyark.layao.common.setDiscountPrice
+import java.util.*
+
+class FavoritesViewHolder internal constructor(private val view: View) :
+    RecyclerView.ViewHolder(view) {
+
+    val favButton: ImageButton = view.findViewById(R.id.fav_button)
+
+    internal fun setImage(image: String, context: Context) {
+        val imageView: ImageView = view.findViewById(R.id.image)
+        Glide.with(context).load(image).placeholder(circularProgressBar(context)).into(imageView)
+    }
+
+    internal fun setTitle(value: String) {
+        val textView: TextView = view.findViewById(R.id.title)
+        textView.text = value
+    }
+
+    @ExperimentalStdlibApi
+    internal fun setBrand(brand: String) {
+        val textView: TextView = view.findViewById(R.id.brand)
+        textView.text = brand.capitalize(Locale.getDefault())
+    }
+
+    internal fun setPrice(price: Double, unit: String, discount: Double) {
+        val textView: TextView = view.findViewById(R.id.price)
+        if (discount > 0) {
+            textView.text = String.format(
+                Locale.getDefault(),
+                "Rs. %.2f/%s",
+                setDiscountPrice(price, discount),
+                unit
+            )
+        } else {
+            textView.text = String.format(Locale.getDefault(), "Rs. %.2f/%s", price, unit)
+        }
+    }
+
 }
