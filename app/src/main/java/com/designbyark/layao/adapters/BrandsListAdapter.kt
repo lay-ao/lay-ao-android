d438db5973e1ad1a832a8716b87dfac5eee2c432
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-28 04:32:10 +0500

    apply data binding in brand list adapter

--- app/src/main/java/com/designbyark/layao/adapters/BrandsListAdapter.kt
+++ app/src/main/java/com/designbyark/layao/adapters/BrandsListAdapter.kt
@@ -1,16 +1,10 @@
 package com.designbyark.layao.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.designbyark.layao.R
-import com.designbyark.layao.common.circularProgressBar
 import com.designbyark.layao.data.Brand
+import com.designbyark.layao.databinding.BodyBrandItemBinding
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
@@ -20,33 +14,27 @@ class BrandsListAdapter internal constructor(
 ) : FirestoreRecyclerAdapter<Brand, BrandsListAdapter.BrandsItemViewHolder>(options) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrandsItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.body_brand_item, parent, false)
-        return BrandsItemViewHolder(view)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = BodyBrandItemBinding.inflate(layoutInflater, parent, false)
+        return BrandsItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: BrandsItemViewHolder, position: Int, model: Brand) {
-        holder.run {
-            setImage(holder.itemView.context, model.image)
-            setTitle(model.title)
-            itemView.setOnClickListener {
-                itemClickListener.mBrandsItemClickListener(snapshots
-                    .getSnapshot(holder.adapterPosition).id)
-            }
+        holder.bind(model)
+        holder.itemView.setOnClickListener {
+            itemClickListener.mBrandsItemClickListener(
+                snapshots
+                    .getSnapshot(holder.adapterPosition).id
+            )
         }
     }
 
-    inner class BrandsItemViewHolder internal constructor(private val view: View) :
-        RecyclerView.ViewHolder(view) {
+    inner class BrandsItemViewHolder internal constructor(private val binding: BodyBrandItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        internal fun setTitle(title: String) {
-            val textView: TextView = view.findViewById(R.id.title)
-            textView.text = title
-        }
-
-        internal fun setImage(context: Context, image: String) {
-            val imageView: ImageView = view.findViewById(R.id.image)
-            Glide.with(context).load(image).placeholder(circularProgressBar(context)).into(imageView)
+        fun bind(brand: Brand) {
+            binding.brand = brand
+            binding.executePendingBindings()
         }
     }
 
