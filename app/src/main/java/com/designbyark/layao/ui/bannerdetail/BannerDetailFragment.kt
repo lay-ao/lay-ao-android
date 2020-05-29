f80b57ce8b5e9b768285262c9565e5fdaaccf603
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-29 05:33:02 +0500

    apply data binding in banner detail fragment

--- app/src/main/java/com/designbyark/layao/ui/bannerdetail/BannerDetailFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/bannerdetail/BannerDetailFragment.kt
@@ -8,16 +8,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.designbyark.layao.R
 import com.designbyark.layao.common.BANNER_COLLECTION
 import com.designbyark.layao.common.formatDate
 import com.designbyark.layao.data.Banner
+import com.designbyark.layao.databinding.FragmentBannerDetailBinding
 import com.designbyark.layao.ui.home.HomeFragment
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_banner_detail.view.*
 
 
 class BannerDetailFragment : Fragment() {
@@ -25,6 +27,7 @@ class BannerDetailFragment : Fragment() {
     private var bannerId: String? = null
 
     private lateinit var promoCode: String
+    private lateinit var binding: FragmentBannerDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,40 +41,35 @@ class BannerDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_banner_detail, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_banner_detail, container, false)
+        binding.detail = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        (requireActivity() as AppCompatActivity).run {
-//            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
-//        }
+        (requireActivity() as AppCompatActivity).run {
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
 
         val firestore = FirebaseFirestore.getInstance()
         val collection = firestore.collection(BANNER_COLLECTION)
         val document = bannerId?.let { collection.document(it) }
 
         getData(view, document)
-
-        view.mPromoCode.setOnClickListener {
-            val clipboard =
-                requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            clipboard.setPrimaryClip(ClipData.newPlainText("promo", promoCode))
-            Toast.makeText(requireContext(), "Promo Code Copied!", Toast.LENGTH_SHORT).show()
-        }
     }
 
     private fun getData(view: View, document: DocumentReference?) {
         document?.get()?.addOnSuccessListener { documentSnapshot ->
             val model = documentSnapshot.toObject(Banner::class.java)
             if (model != null) {
-                Glide.with(requireActivity()).load(model.image).into(view.mImage)
-                view.mTitle.text = model.title
-                view.mDescription.text = model.description
-                view.mValidity.text = model.validity?.let { "Valid till ${formatDate(it)}" }
-                view.mPromoCode.text = String.format("Promo Code: %s", model.code)
+                Glide.with(requireActivity()).load(model.image).into(binding.mImage)
+                binding.mTitle.text = model.title
+                binding.mDescription.text = model.description
+                binding.mValidity.text = model.validity?.let { "Valid till ${formatDate(it)}" }
+                binding.mPromoCode.text = String.format("Promo Code: %s", model.code)
                 promoCode = model.code
             }
         }?.addOnFailureListener { exception ->
@@ -81,6 +79,13 @@ class BannerDetailFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
+    }
+
+    fun copyPromoCode() {
+        val clipboard =
+            requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.setPrimaryClip(ClipData.newPlainText("promo", promoCode))
+        Toast.makeText(requireContext(), "Promo Code Copied!", Toast.LENGTH_SHORT).show()
     }
 
 }
