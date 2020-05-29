ab44c715ea7c8e500ab4559764bf2283784753d1
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-30 01:38:18 +0500

    apply data binding in forgot password fragment

--- app/src/main/java/com/designbyark/layao/ui/forgotpassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/forgotpassword/ForgotPasswordFragment.kt
@@ -4,23 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.designbyark.layao.R
 import com.designbyark.layao.common.LOG_TAG
 import com.designbyark.layao.common.disableInteraction
 import com.designbyark.layao.common.emailValidation
 import com.designbyark.layao.common.enableInteraction
+import com.designbyark.layao.databinding.FragmentForgotPasswordBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_forgot_password.view.*
 
 class ForgotPasswordFragment : Fragment() {
 
-    private var title: Int? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: FragmentForgotPasswordBinding
 
-    private lateinit var navController: NavController
+    private var title: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,66 +34,56 @@ class ForgotPasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        setHasOptionsMenu(true)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_forgot_password, container, false)
+        binding.forgot = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
+
+        auth = FirebaseAuth.getInstance()
 
         (requireActivity() as AppCompatActivity).run {
             supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
-        val changePassword = view.context.resources.getString(R.string.change_password)
+        val changePassword = requireContext().resources.getString(R.string.change_password)
 
         if (title == 0) {
             (requireActivity() as AppCompatActivity).run {
                 supportActionBar?.setTitle(changePassword)
             }
-            view.mTitle.text = changePassword
-            view.mResetPassword.text = changePassword
+            binding.mTitle.text = changePassword
+            binding.mResetPassword.text = changePassword
         } else {
-            view.mTitle.text = view.context.resources.getString(R.string.forgot_password)
+            binding.mTitle.text = view.context.resources.getString(R.string.forgot_password)
         }
+    }
 
-        val auth = FirebaseAuth.getInstance()
-        val navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+    fun resetPassword() {
+        val email = binding.mEmailET.text.toString()
+        if (emailValidation(email, binding.mEmailIL)) return
 
-        view.mResetPassword.setOnClickListener {
-
-            val email = view.mEmailET.text.toString()
-            if (emailValidation(email, view.mEmailIL)) return@setOnClickListener
-
-            disableInteraction(requireActivity(), view.mIncludeProgressBar)
-            auth.sendPasswordResetEmail(email)
-                .addOnCompleteListener { task ->
-                    if (task.isComplete && task.isSuccessful) {
-                        enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                        Log.d(LOG_TAG, "Password Reset Email Sent!")
-                        Snackbar.make(view, "Password Reset Email Sent", Snackbar.LENGTH_LONG)
-                            .show()
-                        navController.navigateUp()
-                    }
-                }.addOnFailureListener { exception ->
-                    Log.e(LOG_TAG, exception.localizedMessage, exception)
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
+        disableInteraction(requireActivity(), binding.mIncludeProgressBar)
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isComplete && task.isSuccessful) {
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+                    Log.d(LOG_TAG, "Password Reset Email Sent!")
+                    Snackbar.make(binding.root, "Password Reset Email Sent", Snackbar.LENGTH_LONG)
+                        .show()
+                    findNavController().navigateUp()
                 }
-        }
+            }.addOnFailureListener { exception ->
+                Log.e(LOG_TAG, exception.localizedMessage, exception)
+                enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> navController.navigateUp()
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
 }
