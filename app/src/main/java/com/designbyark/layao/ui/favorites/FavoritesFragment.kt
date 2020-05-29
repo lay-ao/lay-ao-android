c2a38626476b5c426cfa093aa7de18afbd75d346
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-30 00:24:36 +0500

    apply data binding in favorites fragment

--- app/src/main/java/com/designbyark/layao/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/favorites/FavoritesFragment.kt
@@ -1,14 +1,17 @@
 package com.designbyark.layao.ui.favorites
 
-
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.designbyark.layao.R
 import com.designbyark.layao.adapters.FavoriteAdapter
 import com.designbyark.layao.common.USERS_COLLECTION
 import com.designbyark.layao.data.Favorites
+import com.designbyark.layao.databinding.FragmentFavoritesBinding
+import com.designbyark.layao.databinding.FragmentNoFavoritesBinding
 import com.designbyark.layao.util.MarginItemDecoration
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -16,12 +19,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_favorites.view.*
-import kotlinx.android.synthetic.main.fragment_no_favorites.view.*
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var favoritesCollection: CollectionReference
+    private lateinit var emptyLayout: FragmentNoFavoritesBinding
+    private lateinit var binding: FragmentFavoritesBinding
 
     private var firebaseUser: FirebaseUser? = null
     private var favoriteAdapter: FavoriteAdapter? = null
@@ -30,26 +33,26 @@ class FavoritesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        setHasOptionsMenu(true)
         val firebaseAuth = FirebaseAuth.getInstance()
         firebaseUser = firebaseAuth.currentUser
-
         if (firebaseUser == null) {
-            return inflater.inflate(R.layout.fragment_no_favorites, container, false)
+            emptyLayout =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_no_favorites, container, false)
+            emptyLayout.fav = this
+            return emptyLayout.root
         }
 
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorites, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
 
-//        (requireActivity() as AppCompatActivity).run {
-//            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
-//            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//            supportActionBar?.setSubtitle("Prices may subject to change.")
-//        }
+        (requireActivity() as AppCompatActivity).run {
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
 
         val bottomMenu: BottomNavigationView = requireActivity().findViewById(R.id.bottom_nav_view)
         if (bottomMenu.visibility == View.GONE) {
@@ -57,14 +60,6 @@ class FavoritesFragment : Fragment() {
         }
 
         if (firebaseUser == null) {
-            view.mLoginAuth.setOnClickListener {
-                Navigation.createNavigateOnClickListener(R.id.action_favoritesFragment_to_signInFragment)
-            }
-
-            view.mRegisterAuth.setOnClickListener {
-                Navigation.createNavigateOnClickListener(R.id.action_favoritesFragment_to_signUpFragment)
-            }
-
             return
         }
 
@@ -76,11 +71,9 @@ class FavoritesFragment : Fragment() {
             .setQuery(favoritesCollection, Favorites::class.java)
             .build()
 
-        favoriteAdapter = FavoriteAdapter(
-            options,
-            favoritesCollection
-        )
-        view.mFavoritesRV.let {
+        favoriteAdapter = FavoriteAdapter(options, favoritesCollection)
+
+        binding.mFavoritesRV.let {
             it.adapter = favoriteAdapter
             it.addItemDecoration(
                 MarginItemDecoration(
@@ -90,11 +83,12 @@ class FavoritesFragment : Fragment() {
         }
     }
 
-    override fun onDestroyView() {
-//        (requireActivity() as AppCompatActivity).run {
-//            supportActionBar?.setSubtitle(null)
-//        }
-        super.onDestroyView()
+    fun login() {
+        findNavController().navigate(R.id.action_favoritesFragment_to_signInFragment)
+    }
+
+    fun register() {
+        findNavController().navigate(R.id.action_favoritesFragment_to_signUpFragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
