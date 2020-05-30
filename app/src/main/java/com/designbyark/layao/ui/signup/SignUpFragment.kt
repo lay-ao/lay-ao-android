dc424ec4e9a408dda29eff05d463d467c46dff83
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-31 04:34:32 +0500

    apply data binding in sign up fragment

--- app/src/main/java/com/designbyark/layao/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/signup/SignUpFragment.kt
@@ -3,22 +3,21 @@ package com.designbyark.layao.ui.signup
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.designbyark.layao.R
 import com.designbyark.layao.common.*
 import com.designbyark.layao.data.User
+import com.designbyark.layao.databinding.FragmentSignUpBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
-import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 class SignUpFragment : Fragment() {
 
@@ -26,92 +25,89 @@ class SignUpFragment : Fragment() {
         const val KEY = "USER_KEY"
     }
 
-    private lateinit var navController: NavController
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        setHasOptionsMenu(true)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false)
+        binding.signup = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-//        (requireActivity() as AppCompatActivity).run {
-//            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        }
-//        setHasOptionsMenu(true)
+        (requireActivity() as AppCompatActivity).run {
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
 
         val bottomMenu: BottomNavigationView = requireActivity().findViewById(R.id.bottom_nav_view)
         bottomMenu.visibility = View.GONE
-
-        navController = Navigation.findNavController(
-            requireActivity(),
-            R.id.nav_host_fragment
-        )
-
-        view.mSignUp.setOnClickListener {
-
-            if (!isConnectedToInternet(requireContext())) {
-                Log.e(LOG_TAG, "Not connected to the internet!")
-                return@setOnClickListener
-            }
-
-            disableInteraction(requireActivity(), view.mIncludeProgressBar)
-
-            startRegistrationProcess(view, auth)
-        }
     }
 
-    private fun startRegistrationProcess(view: View, auth: FirebaseAuth) {
-
-        val fullName = view.mFullNameET.text.toString()
-        if (emptyValidation(fullName, view.mFullNameIL)) {
-            enableInteraction(requireActivity(), view.mIncludeProgressBar)
+    fun signUp() {
+        if (!isConnectedToInternet(requireContext())) {
+            Log.e(LOG_TAG, "Not connected to the internet!")
             return
         }
 
-        val email = view.mEmailET.text.toString()
-        if (emailValidation(email, view.mEmailIL)) {
-            enableInteraction(requireActivity(), view.mIncludeProgressBar)
+        disableInteraction(requireActivity(), binding.mIncludeProgressBar)
+
+        startRegistrationProcess()
+    }
+
+    private fun startRegistrationProcess() {
+
+        val fullName = binding.mFullNameET.text.toString()
+        if (emptyValidation(fullName, binding.mFullNameIL)) {
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
             return
         }
 
-        val password = view.mPasswordET.text.toString()
-        if (passwordValidation(password, view.mPasswordIL)) {
-            enableInteraction(requireActivity(), view.mIncludeProgressBar)
+        val email = binding.mEmailET.text.toString()
+        if (emailValidation(email, binding.mEmailIL)) {
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
             return
         }
 
-        val confirmPassword = view.mConfirmPassET.text.toString()
+        val password = binding.mPasswordET.text.toString()
+        if (passwordValidation(password, binding.mPasswordIL)) {
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+            return
+        }
+
+        val confirmPassword = binding.mConfirmPassET.text.toString()
         if (confirmPasswordValidation(
                 confirmPassword,
                 password,
-                view.mConfirmPassIL
+                binding.mConfirmPassIL
             )
         ) {
-            enableInteraction(requireActivity(), view.mIncludeProgressBar)
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
             return
         }
 
-        if (!view.mTerms.isChecked) {
+        if (!binding.mTerms.isChecked) {
             Toast.makeText(
                 requireContext(),
                 "Kindly, accept our Terms & Condition before proceeding", Toast.LENGTH_SHORT
             ).show()
-            view.mTermsText.setTextColor(
+            binding.mTermsText.setTextColor(
                 ContextCompat.getColor(
                     requireContext(),
                     android.R.color.holo_red_dark
                 )
             )
-            enableInteraction(requireActivity(), view.mIncludeProgressBar)
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
             return
         } else {
-            view.mTermsText.setTextColor(
+            binding.mTermsText.setTextColor(
                 ContextCompat.getColor(
                     requireContext(),
                     android.R.color.holo_blue_dark
@@ -131,23 +127,25 @@ class SignUpFragment : Fragment() {
                 if (user != null) {
                     userModel.userId = user.uid
                     Log.d(LOG_TAG, "createUserWithEmailAndPassword: Started!")
-                    updateAuthProfile(view, fullName, user, userModel)
+                    updateAuthProfile(fullName, user, userModel)
                 } else {
                     Log.e(
                         LOG_TAG,
                         "createUserWithEmailAndPassword -> addOnCompleteListener -> currentUser: user is null"
                     )
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
                     return@addOnCompleteListener
                 }
             }
         }
     }
 
-    private fun updateAuthProfile(view: View, fullName: String, user: FirebaseUser, userModel: User) {
+    private fun updateAuthProfile(fullName: String, user: FirebaseUser, userModel: User) {
+
         val profileUpdates = UserProfileChangeRequest.Builder()
             .setDisplayName(fullName)
             .build()
+
         user.updateProfile(profileUpdates)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful && task.isComplete) {
@@ -155,13 +153,13 @@ class SignUpFragment : Fragment() {
                     Log.d(LOG_TAG, "updateProfile: Started!")
                     val args = Bundle()
                     args.putSerializable(KEY, userModel)
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                    navController.navigate(
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+                    findNavController().navigate(
                         R.id.action_signUpFragment_to_signUpDetailsFragment,
                         args
                     )
                 } else {
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
                     Log.e(
                         LOG_TAG,
                         "updateAuthProfile -> updateProfile -> addOnCompleteListener: " +
@@ -171,7 +169,7 @@ class SignUpFragment : Fragment() {
                 }
             }
             .addOnFailureListener {
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                enableInteraction(requireActivity(), binding.mIncludeProgressBar)
                 Log.d(
                     LOG_TAG,
                     "updateAuthProfile -> updateProfile -> addOnFailureListener: " +
@@ -182,16 +180,9 @@ class SignUpFragment : Fragment() {
             }
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            android.R.id.home -> navController.navigateUp()
-//            else -> return super.onOptionsItemSelected(item)
-//        }
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        menu.clear()
-//    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+    }
 
 }
 
