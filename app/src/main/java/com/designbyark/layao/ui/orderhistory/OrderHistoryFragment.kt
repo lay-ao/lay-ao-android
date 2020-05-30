9f2089fb5d3960696b025e2451ec57e7fd801411
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-31 02:43:24 +0500

    apply data binding in order history fragment

--- app/src/main/java/com/designbyark/layao/ui/orderhistory/OrderHistoryFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/orderhistory/OrderHistoryFragment.kt
@@ -4,38 +4,39 @@ package com.designbyark.layao.ui.orderhistory
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.designbyark.layao.R
 import com.designbyark.layao.adapters.OrderAdapter
 import com.designbyark.layao.common.ORDERS_COLLECTION
 import com.designbyark.layao.data.Order
+import com.designbyark.layao.databinding.FragmentOrderHistoryBinding
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_order_history.view.*
 
 class OrderHistoryFragment : Fragment(),
     OrderAdapter.OrderItemClickListener {
 
     private var adapter: OrderAdapter? = null
+    private lateinit var binding: FragmentOrderHistoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_order_history, container, false)
+        setHasOptionsMenu(true)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_order_history, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
 
         (requireActivity() as AppCompatActivity).run {
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
             supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
         }
-        setHasOptionsMenu(true)
 
         val firebaseAuth = FirebaseAuth.getInstance()
         val firebaseUser = firebaseAuth.currentUser
@@ -45,13 +46,13 @@ class OrderHistoryFragment : Fragment(),
 
         val query = orderCollection.whereEqualTo("userId", firebaseUser!!.uid)
             .whereGreaterThanOrEqualTo("orderStatus", 5)
+
         val options = FirestoreRecyclerOptions.Builder<Order>()
             .setQuery(query, Order::class.java)
             .build()
 
         adapter = OrderAdapter(options, this)
-
-        view.mHistoryOrdersRV.adapter = adapter
+        binding.mHistoryOrdersRV.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -77,7 +78,7 @@ class OrderHistoryFragment : Fragment(),
     override fun orderItemClickListener(orderId: String) {
         val args = Bundle()
         args.putString("orderId", orderId)
-        Navigation.createNavigateOnClickListener(
+        findNavController().navigate(
             R.id.action_orderHistoryFragment_to_orderDetailFragment,
             args
         )
