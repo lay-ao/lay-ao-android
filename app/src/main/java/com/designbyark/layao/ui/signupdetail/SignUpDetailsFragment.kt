7263e4e73b13c5579702ec4974e823f5d2476e66
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-31 17:19:59 +0500

    apply data binding in sign up details fragment

--- app/src/main/java/com/designbyark/layao/ui/signupdetail/SignUpDetailsFragment.kt
+++ app/src/main/java/com/designbyark/layao/ui/signupdetail/SignUpDetailsFragment.kt
@@ -7,18 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.designbyark.layao.R
 import com.designbyark.layao.common.*
 import com.designbyark.layao.data.User
+import com.designbyark.layao.databinding.FragmentSignUpDetailsBinding
 import com.designbyark.layao.ui.signup.SignUpFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_sign_up_details.view.*
 
 class SignUpDetailsFragment : Fragment() {
 
@@ -26,7 +26,7 @@ class SignUpDetailsFragment : Fragment() {
     private var user: User? = null
 
     private lateinit var userCollection: CollectionReference
-    private lateinit var navController: NavController
+    private lateinit var binding: FragmentSignUpDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +40,10 @@ class SignUpDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_sign_up_details, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up_details, container, false)
+        binding.detail = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,83 +52,17 @@ class SignUpDetailsFragment : Fragment() {
         firebaseAuth = FirebaseAuth.getInstance()
         val firestore = FirebaseFirestore.getInstance()
         userCollection = firestore.collection(USERS_COLLECTION)
-
-        navController = Navigation.findNavController(
-            requireActivity(),
-            R.id.nav_host_fragment
-        )
-
-        view.mSkip.setOnClickListener {
-
-            // Disable Interaction
-            disableInteraction(requireActivity(), view.mIncludeProgressBar)
-
-            // Set Default values
-            user?.houseNumber = ""
-            user?.blockNumber = 0
-            user?.completeAddress = ""
-            user?.gender = 0
-            user?.contact = ""
-            user?.fineCount = 0
-
-            // Store user at Firestore
-            Log.d(LOG_TAG, "saveUserAtFirestore (Skip): Started")
-            saveUserAtFirestore(view, firebaseAuth.currentUser!!)
-
-        }
-
-        view.mDone.setOnClickListener {
-
-            // Disable Interaction
-            disableInteraction(requireActivity(), view.mIncludeProgressBar)
-
-            // Validate fields
-            val phoneNumber = view.mContactET.text.toString().trim()
-            val houseNumber = view.mHouseNumET.text.toString().trim()
-
-            if (phoneValidation(phoneNumber, view.mContactIL)) {
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                return@setOnClickListener
-            }
-            if (emptyValidation(houseNumber, view.mHouseNumIL)) {
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                return@setOnClickListener
-            }
-
-            if (view.mBlockSpinner.selectedItemPosition == 0) {
-                Toast.makeText(requireContext(), "Invalid block selected", Toast.LENGTH_LONG)
-                    .show()
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                return@setOnClickListener
-            }
-
-            // Set values
-            user?.houseNumber = houseNumber
-            user?.blockNumber = view.mBlockSpinner.selectedItemPosition
-            user?.completeAddress = String.format(
-                "House #%s, %s, Wapda Town, Lahore",
-                houseNumber,
-                view.mBlockSpinner.selectedItem.toString()
-            )
-            user?.gender = view.mGenderSpinner.selectedItemPosition
-            user?.contact = phoneNumber
-            user?.fineCount = 0
-
-            // Start Registration Process
-            Log.d(LOG_TAG, "saveUserAtFirestore (Done): Started")
-            saveUserAtFirestore(view, firebaseAuth.currentUser!!)
-        }
     }
 
-    private fun saveUserAtFirestore(view: View, firebaseUser: FirebaseUser) {
+    private fun saveUserAtFirestore(firebaseUser: FirebaseUser) {
         userCollection.document(firebaseUser.uid)
             .set(user!!)
             .addOnCompleteListener { task ->
                 if (task.isComplete && task.isSuccessful) {
-                    sendVerificationEmail(view)
+                    sendVerificationEmail()
 
                 } else {
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
                     Log.e(
                         LOG_TAG,
                         "saveUserAtFirestore -> addOnCompleteListener: task is neither complete nor successful"
@@ -135,7 +71,7 @@ class SignUpDetailsFragment : Fragment() {
                 }
             }
             .addOnFailureListener {
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                enableInteraction(requireActivity(), binding.mIncludeProgressBar)
                 Log.e(
                     LOG_TAG,
                     "saveUserAtFirestore -> addOnFailureListener: ${it.localizedMessage}",
@@ -145,18 +81,76 @@ class SignUpDetailsFragment : Fragment() {
             }
     }
 
-    private fun sendVerificationEmail(view: View) {
+    fun skip() {
+        // Disable Interaction
+        disableInteraction(requireActivity(), binding.mIncludeProgressBar)
+
+        // Set Default values
+        user?.houseNumber = ""
+        user?.blockNumber = 0
+        user?.completeAddress = ""
+        user?.gender = 0
+        user?.contact = ""
+        user?.fineCount = 0
+
+        // Store user at Firestore
+        Log.d(LOG_TAG, "saveUserAtFirestore (Skip): Started")
+        saveUserAtFirestore(firebaseAuth.currentUser!!)
+    }
+
+    fun completeRegistration() {
+        // Disable Interaction
+        disableInteraction(requireActivity(), binding.mIncludeProgressBar)
+
+        // Validate fields
+        val phoneNumber = binding.mContactET.text.toString().trim()
+        val houseNumber = binding.mHouseNumET.text.toString().trim()
+
+        if (phoneValidation(phoneNumber, binding.mContactIL)) {
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+            return
+        }
+        if (emptyValidation(houseNumber, binding.mHouseNumIL)) {
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+            return
+        }
+
+        if (binding.mBlockSpinner.selectedItemPosition == 0) {
+            Toast.makeText(requireContext(), "Invalid block selected", Toast.LENGTH_LONG)
+                .show()
+            enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+            return
+        }
+
+        // Set values
+        user?.houseNumber = houseNumber
+        user?.blockNumber = binding.mBlockSpinner.selectedItemPosition
+        user?.completeAddress = String.format(
+            "House #%s, %s, Wapda Town, Lahore",
+            houseNumber,
+            binding.mBlockSpinner.selectedItem.toString()
+        )
+        user?.gender = binding.mGenderSpinner.selectedItemPosition
+        user?.contact = phoneNumber
+        user?.fineCount = 0
+
+        // Start Registration Process
+        Log.d(LOG_TAG, "saveUserAtFirestore (Done): Started")
+        saveUserAtFirestore(firebaseAuth.currentUser!!)
+    }
+
+    private fun sendVerificationEmail() {
         val user = firebaseAuth.currentUser
         user?.sendEmailVerification()
             ?.addOnCompleteListener { task ->
                 if (task.isComplete && task.isSuccessful) {
                     Log.d(LOG_TAG, "Email Sent")
-                    enableInteraction(requireActivity(), view.mIncludeProgressBar)
-                    navController.navigate(R.id.action_signUpDetailsFragment_to_navigation_user)
+                    enableInteraction(requireActivity(), binding.mIncludeProgressBar)
+                    findNavController().navigate(R.id.action_signUpDetailsFragment_to_navigation_user)
                 }
             }?.addOnFailureListener { exception ->
                 Log.e(LOG_TAG, exception.localizedMessage, exception)
-                enableInteraction(requireActivity(), view.mIncludeProgressBar)
+                enableInteraction(requireActivity(), binding.mIncludeProgressBar)
             }
     }
 
